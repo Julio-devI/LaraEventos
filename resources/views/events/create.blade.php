22f61748cd1661d5ae11a7499360b354d5ad88e6
julio <julioakzzo@hotmail.com>
2021-07-08 09:06:46 -0300

    initial commit

---
+++ resources/views/events/create.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('title', 'Criar evento')
+
+@section('contente')
+
+<h1> Crie um Evento </h1>
+
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,33 @@
+<body class="antialiased">
+    @extends('layouts.main')
+
+    @section('title', 'HDC events')
+    
+    @section('content')
+    
+    <div id="search-container" class="col-md-12">
+        <h1>Busque um evento</h1>
+        <form>
+            <input type="text" id="search" name="search" class="form-control" placeholder="Procurar">
+        </form>
+    </div>
+
+    <div id="events-container" class="col-md-12">
+        <h2> Próximos Eventos </h2>
+        <p> Veja os eventos dos proximos dias</p>
+        <div id="cards-container" class="roll">
+            @foreach($events as $event)
+                <div class="card col-md-3">
+                    <img src="/img/eventos.jpg" alt="{{ $event->title }}">
+                    <div class="card-body">
+                        <p class="card-date">10/09/2021</p>
+                        <h5 class="card-title">{{ $event->title }}</h5>
+                        <p class="card-participants"> x-participants </p>
+                        <a href="#" class="btn btn-primary"> Saber mais </a>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+
+    @endsection
